b640b40e8eb97fe132b7538f796787125d7f6df3
Fellipe <araujo.fellipe@hotmail.com>
2020-12-31 13:51:41 -0300

    Create Map Page.

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import { View, Text, Image, ImageBackground } from "react-native";
 import { RectButton } from 'react-native-gesture-handler';
 import { Feather as Icon } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import styles from "./styles";
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  const handleNavigateToPoints = () => {
+    navigation.navigate('Points');
+  };
+
   return (
     <ImageBackground
       source={require("../../assets/home-background.png")}
@@ -20,7 +27,7 @@ const Home = () => {
       </View>
 
       <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={() => {}}>
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
           <View style={styles.buttonIcon}>
             <Icon name="arrow-right" color="#FFF" size={24} />
           </View>
